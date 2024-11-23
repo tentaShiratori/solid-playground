8c8ed29550b9c976ae3c1c1e345d6faa4783712d
tentaShiratori <tenta.shiratori@gmail.com>
2024-11-23 21:06:52 +0900

    msw and biome

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,19 @@
 // @refresh reload
-import { mount, StartClient } from "@solidjs/start/client";
+import { StartClient, mount } from "@solidjs/start/client";
 
-mount(() => <StartClient />, document.getElementById("app")!);
+async function enableMocking() {
+	if (process.env.NODE_ENV !== "development") {
+		return;
+	}
+
+	const { worker } = await import("./mocks/browser");
+
+	// `worker.start()` returns a Promise that resolves
+	// once the Service Worker is up and ready to intercept requests.
+	return worker.start();
+}
+
+enableMocking().then(() =>
+	// biome-ignore lint/style/noNonNullAssertion: <explanation>
+	mount(() => <StartClient />, document.getElementById("app")!),
+);
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,37 @@
 import { Title } from "@solidjs/meta";
+import { onMount, Show } from "solid-js";
 import Counter from "~/components/Counter";
+import { createAsync, query } from "@solidjs/router";
+
+type User = { id:string,firstName:string,lastName:string };
+
+const getUser = query(async () => {
+	console.log("Fetching user...");
+	const response = await fetch("https://example.com/user");
+	return (await response.json()) as User;
+}, "user");
+
+export const route = {
+	preload: () => getUser(),
+};
 
 export default function Home() {
-  return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
-    </main>
-  );
+		const user = createAsync(() => getUser());
+	return (
+		<main>
+			<Title>Hello World</Title>
+			<h1>Hello world!</h1>
+			<Counter />
+			<Show when={user()}>{(u)=>{
+				return <p>User: {u().firstName} {u().lastName}</p>
+			}}</Show>
+			<p>
+				Visit{" "}
+				<a href="https://start.solidjs.com" target="_blank" rel="noreferrer">
+					start.solidjs.com
+				</a>{" "}
+				to learn how to build SolidStart apps.
+			</p>
+		</main>
+	);
 }
