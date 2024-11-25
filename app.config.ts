9f73f588b86f0273127c567b6d64e919fb41855e
tentaShiratori <tenta.shiratori@gmail.com>
2024-11-25 21:08:47 +0900

    uiライブラリ

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
+import suidPlugin from "@suid/vite-plugin";
 
 export default defineConfig({
 	middleware: "./src/middleware.ts",
+	vite: {
+		plugins: [suidPlugin()],
+	},
 });
---
+++ src/routes/ui/suid.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@suid/material";
+
+export default function Page() {
+	return (
+		<div>
+			<Button variant="contained">Hello World</Button>
+		</div>
+	);
+}
